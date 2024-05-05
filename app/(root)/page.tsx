dc8905273f1d25efa1904c39f27a829fbbf0ff2c
Akaikenlol <keithken972@gmail.com>
2024-05-05 20:33:05 +0630

    feat: Home Page UI chore: npm install, chartjs, query-string

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-	return <div>Home</div>;
+	const loggedIn = { firstName: "Keith" };
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Welcome"
+						user={loggedIn.firstName || "GUST"}
+						subtext="Access and manage your account and transactions efficiently"
+					/>
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={1250.35}
+					/>
+				</header>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
