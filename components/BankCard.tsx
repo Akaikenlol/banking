8241c11f8c49874fd128f17e8a1ee40947421d89
Akaikenlol <keithken972@gmail.com>
2024-05-18 20:05:37 +0630

    feat: Pagination and Spending Categories chore: npm install progress

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -14,7 +14,7 @@ const BankCard = ({ account, userName, showBalance }: CreditCardProps) => {
 			>
 				<div className="bank-card_content">
 					<div>
-						<h1 className="text-16 font-semibold text-white">{userName}</h1>
+						<h1 className="text-16 font-semibold text-white">{account.name}</h1>
 						<p className="font-ibm-plex-serif font-black text-white">
 							{formatAmount(account.currentBalance)}
 						</p>
